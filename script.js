ffd36d222a8f5d09547d51c89801916e4afac8ac
Sonia Craciun <bysoniiaaa@gmail.com>
2022-01-25 18:17:04 -0800

    added variables for the hours and function for date and hour

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 // Moment.js
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
+// Text hour var
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+// var hourString = $(".hour").text().split(" ");
+
+// Date and Hour
+
+var interval = setInterval(function() {
+  var momentNow = moment();
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                      + momentNow.format('dddd')
+                       .substring(0,3).toUpperCase());
+  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
